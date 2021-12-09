3a5a36246f8d7c892ca46d1b9a72827269067ac8
Utkarsh Vashishtha <utkarsh.vashishtha1997@gmail.com>
2021-12-09 15:08:04 -0800

    Integrating.. in the middle of it!

--- src/UI.hs
+++ src/UI.hs
@@ -34,8 +34,11 @@ import Graphics.Vty
 import Control.Lens
 import Brick.Widgets.Border.Style
 import System.IO
+import Data.Map (Map, empty)
+import Epsilon.Evaluator
+import Epsilon.Types
 
-data Name =     CodeView 
+data Name =     CodeView
             |   StackView
             |   VariablesView
             |   CommandsView
@@ -46,7 +49,9 @@ data ProgState = ProgState {
     codeLines :: [(String, Int)], -- (line of code, line number)
     selectedElement :: Int, -- The currently selected element in the CodeView.
     breakpoints :: Set Int, -- contains "breakpointed" line numbers
-    commandsViewContent :: String -- This is what goes inside the "Help" View
+    commandsViewContent :: String, -- This is what goes inside the "Help" View
+    evalState :: DState, -- The state of the evaluator
+    evaluator :: Epsilon Value -- The evaluator monad
 }
 
 Lens.Micro.TH.makeLenses ''ProgState
@@ -59,19 +64,19 @@ drawUi ps = case F.focusGetCurrent (ps^.focusRing) of
                 Just CommandsView -> [buildViewOf 3]
                 Nothing -> error "Illegal State Error"
                 where
-                    svw = str "This will have the Stack contents"
-                    vvw = str "This will have the Variables and their values"
+                    svw = manageStackView ps
+                    vvw = manageVariablesView ps
                     comvw = str $ commandsViewContent ps
                     buildViewOf = buildViewUI ps svw vvw comvw
 
 buildViewUI :: ProgState -> Widget Name -> Widget Name -> Widget Name -> Int -> Widget Name
-buildViewUI ps stackViewWidget varViewWidget comViewWidget viewType = 
-    C.center $ withBorderStyle unicodeBold $ B.borderWithLabel headLabel $ hBox [ vBox [ withBorderStyle unicodeBold $ B.hBorderWithLabel (withAttr headerLabelScheme $ str "Code View"), 
+buildViewUI ps stackViewWidget varViewWidget comViewWidget viewType =
+    C.center $ withBorderStyle unicodeBold $ B.borderWithLabel headLabel $ hBox [ vBox [ withBorderStyle unicodeBold $ B.hBorderWithLabel (withAttr headerLabelScheme $ str "Code View"),
         withAttr codePanelBg codePanel ], withBorderStyle unicodeBold B.vBorder, miscPanel]
     where
         headLabel = withAttr headerLabelScheme $ str "THE DELTA DEBUGGER FOR EPSILON"
         codePanel = viewport CodeView Both $ vBox $ assembleCodeViewWidgets ps $ viewType == 0
-        miscPanel = vBox [ 
+        miscPanel = vBox [
                             withBorderStyle unicodeBold $ B.hBorderWithLabel (withAttr headerLabelScheme $ str "Stack View"),
                             withAttr (if viewType == 1 then miscPanelSelectedScheme else miscPanelScheme) $ viewport StackView Both stackViewWidget,
                             withBorderStyle unicodeBold $ B.hBorderWithLabel (withAttr headerLabelScheme $ str "Variables View"),
@@ -80,16 +85,25 @@ buildViewUI ps stackViewWidget varViewWidget comViewWidget viewType =
                             withAttr (if viewType == 3 then miscPanelSelectedScheme else miscPanelScheme) $ viewport CommandsView Both comViewWidget
                          ]
 
+manageStackView :: ProgState -> Widget n
+manageStackView ps = error ""
+
+manageVariablesView :: ProgState -> Widget n
+manageVariablesView ps = error ""
+
 assembleCodeViewWidgets :: ProgState -> Bool -> [Widget n]
-assembleCodeViewWidgets ps isCodeView = map (\ (codeline, line) -> 
-                            if member line (breakpoints ps) then 
-                                withAttr breakpointScheme $ str $ show line ++ (if line < 10 then ".  " else ". ") ++ codeline
+assembleCodeViewWidgets ps isCodeView = map (\ (codeline, line) ->
+                            if member line (breakpoints ps) then
+                                if line == Epsilon.Evaluator.loc (evalState ps) then
+                                    withAttr executionLineScheme $ str $ show line ++ (if line < 10 then ".  " else ". ") ++ codeline
+                                else
+                                    withAttr breakpointScheme $ str $ show line ++ (if line < 10 then ".  " else ". ") ++ codeline
                             else
                                 if line == selectedElement ps && isCodeView then
                                     withAttr codePanelSelectedScheme $ str $ show line ++ (if line < 10 then ".  " else ". ") ++ codeline
-                                else     
+                                else
                                     withAttr codePanelScheme $ str $ show line ++ (if line < 10 then ".  " else ". ") ++ codeline
-                         ) 
+                         )
                      (codeLines ps)
 
 cvScroll :: M.ViewportScroll Name
@@ -109,7 +123,7 @@ appCursor = F.focusRingCursor (^.focusRing)
 
 -- We may need, if we want of course, to check for conditions that are not valid inputs etc.
 appEvent :: ProgState -> T.BrickEvent Name e -> T.EventM Name (T.Next ProgState)
-appEvent ps (T.VtyEvent ev) = case ev of 
+appEvent ps (T.VtyEvent ev) = case ev of
     V.EvKey V.KEsc [] -> M.halt ps
     V.EvKey (V.KChar '\t') [] -> M.continue $ ps & focusRing %~ F.focusNext
     V.EvKey V.KBackTab [] -> M.continue $ ps & focusRing %~ F.focusPrev
@@ -153,35 +167,140 @@ appEvent ps (T.VtyEvent ev) = case ev of
         Just CodeView -> M.continue $ updateBreakPointSet ps
         _ -> M.continue ps
     V.EvKey (V.KChar 'r') [] -> case F.focusGetCurrent (ps^.focusRing) of
-        Just CodeView -> error "This is where the control gets transferred to the AST editor method where we insert 'breaks' in the AST and call"
-                                "the evaluator/interpreter for the first time and update the ProgState"
+        Just CodeView -> M.continue $ startEvaluation ps
         _ -> M.continue ps
     V.EvKey (V.KChar 'n') [] -> case F.focusGetCurrent (ps^.focusRing) of
-        Just CodeView -> error "Execute the evaluator till the next breakpoint/end."
+        Just CodeView -> M.continue $ nextEvaluation ps
+        _ -> M.continue ps
+    V.EvKey (V.KChar 's') [] -> case F.focusGetCurrent (ps^.focusRing) of
+        Just CodeView -> M.continue $ stepEvaluation ps
+        _ -> M.continue ps
+    V.EvKey (V.KChar 'o') [] -> case F.focusGetCurrent (ps^.focusRing) of
+        Just CodeView -> error "step over may or may not be supported"
         _ -> M.continue ps
     _ -> M.continue ps
 appEvent ps _ = M.continue ps
 
+startEvaluation :: ProgState -> ProgState
+startEvaluation ps = case snd $ startEpsilon (parseAndBuildAST ps) of
+                        Left merror -> case merror of
+                                    Just _ ->  ProgState { -- Do something here
+                                                _focusRing = ps^.focusRing,
+                                                codeLines = codeLines ps,
+                                                selectedElement = selectedElement ps,
+                                                breakpoints = breakpoints ps,
+                                                commandsViewContent = commandsViewContent ps,
+                                                evalState = evalState ps,
+                                                evaluator = evaluator ps
+                                            }
+                                    Nothing ->  ProgState { -- Do something here
+                                                    _focusRing = ps^.focusRing,
+                                                    codeLines = codeLines ps,
+                                                    selectedElement = selectedElement ps,
+                                                    breakpoints = breakpoints ps,
+                                                    commandsViewContent = commandsViewContent ps,
+                                                    evalState = evalState ps,
+                                                    evaluator = evaluator ps
+                                                }
+                        Right st -> ProgState {
+                                                _focusRing = ps^.focusRing,
+                                                codeLines = codeLines ps,
+                                                selectedElement = selectedElement ps,
+                                                breakpoints = breakpoints ps,
+                                                commandsViewContent = commandsViewContent ps,
+                                                evalState = fst st,
+                                                evaluator = snd st
+                                    }
+
+nextEvaluation :: ProgState -> ProgState
+nextEvaluation ps = case snd $ continueEpsilon (evaluator ps) (state $ evalState ps) of
+                        Left merror -> case merror of
+                                    Just _ ->  ProgState { -- Do something here
+                                                _focusRing = ps^.focusRing,
+                                                codeLines = codeLines ps,
+                                                selectedElement = selectedElement ps,
+                                                breakpoints = breakpoints ps,
+                                                commandsViewContent = commandsViewContent ps,
+                                                evalState = evalState ps,
+                                                evaluator = evaluator ps
+                                            }
+                                    Nothing ->  ProgState { -- Do something here
+                                                    _focusRing = ps^.focusRing,
+                                                    codeLines = codeLines ps,
+                                                    selectedElement = selectedElement ps,
+                                                    breakpoints = breakpoints ps,
+                                                    commandsViewContent = commandsViewContent ps,
+                                                    evalState = evalState ps,
+                                                    evaluator = evaluator ps
+                                                }
+                        Right st -> ProgState {
+                                                _focusRing = ps^.focusRing,
+                                                codeLines = codeLines ps,
+                                                selectedElement = selectedElement ps,
+                                                breakpoints = breakpoints ps,
+                                                commandsViewContent = commandsViewContent ps,
+                                                evalState = fst st,
+                                                evaluator = snd st
+                                    }
+
+stepEvaluation :: ProgState -> ProgState
+stepEvaluation ps = case snd $ stepEpsilon (evaluator ps) (state $ evalState ps) of
+                        Left merror -> case merror of
+                                    Just _ ->  ProgState { -- Do something here
+                                                _focusRing = ps^.focusRing,
+                                                codeLines = codeLines ps,
+                                                selectedElement = selectedElement ps,
+                                                breakpoints = breakpoints ps,
+                                                commandsViewContent = commandsViewContent ps,
+                                                evalState = evalState ps,
+                                                evaluator = evaluator ps
+                                            }
+                                    Nothing ->  ProgState { -- Do something here
+                                                    _focusRing = ps^.focusRing,
+                                                    codeLines = codeLines ps,
+                                                    selectedElement = selectedElement ps,
+                                                    breakpoints = breakpoints ps,
+                                                    commandsViewContent = commandsViewContent ps,
+                                                    evalState = evalState ps,
+                                                    evaluator = evaluator ps
+                                                }
+                        Right st -> ProgState {
+                                                _focusRing = ps^.focusRing,
+                                                codeLines = codeLines ps,
+                                                selectedElement = selectedElement ps,
+                                                breakpoints = breakpoints ps,
+                                                commandsViewContent = commandsViewContent ps,
+                                                evalState = fst st,
+                                                evaluator = snd st
+                                    }
+
+parseAndBuildAST :: ProgState -> Statement
+parseAndBuildAST ps = prog1
+
 updateBreakPointSet :: ProgState -> ProgState
 updateBreakPointSet ps = ProgState {
-                                    _focusRing = ps^.focusRing, 
-                                    codeLines = codeLines ps, 
+                                    _focusRing = ps^.focusRing,
+                                    codeLines = codeLines ps,
                                     selectedElement = selectedElement ps,
                                     breakpoints = updatedBPSet,
-                                    commandsViewContent = commandsViewContent ps
+                                    commandsViewContent = commandsViewContent ps,
+                                    evalState = evalState ps,
+                                    evaluator = evaluator ps
                                 }
                                 where updatedBPSet = if member (selectedElement ps) (breakpoints ps) then
                                                         delete (selectedElement ps) (breakpoints ps)
-                                                     else 
+                                                     else
                                                         insert (selectedElement ps) (breakpoints ps)
 
 updateSelectedElement :: ProgState -> Int -> ProgState
 updateSelectedElement ps c = ProgState {
-                                    _focusRing = ps^.focusRing, 
-                                    codeLines = codeLines ps, 
+                                    _focusRing = ps^.focusRing,
+                                    codeLines = codeLines ps,
                                     selectedElement = min (max 1 (selectedElement ps + c)) maxLine,
                                     breakpoints = breakpoints ps,
-                                    commandsViewContent = commandsViewContent ps
+                                    commandsViewContent = commandsViewContent ps,
+                                    evalState = evalState ps,
+                                    evaluator = evaluator ps
                                 }
                                 where maxLine = snd $ last (codeLines ps)
 
@@ -196,7 +315,7 @@ app =
           }
 
 codePanelScheme, headerLabelScheme, codePanelBg, codePanelSelectedScheme, miscPanelScheme :: AttrName
-miscPanelSelectedScheme, breakpointScheme :: AttrName
+miscPanelSelectedScheme, breakpointScheme, executionLineScheme :: AttrName
 codePanelScheme = attrName "code-panel-scheme"
 codePanelBg = attrName "code-panel-bg"
 codePanelSelectedScheme = attrName "code-panel-selected-scheme"
@@ -204,6 +323,7 @@ miscPanelScheme = attrName "misc-panel-scheme"
 miscPanelSelectedScheme = attrName "misc-panel-selected-scheme"
 breakpointScheme = attrName "breakpoint-scheme"
 headerLabelScheme = attrName "header-label-scheme"
+executionLineScheme = attrName "execution-line-scheme"
 
 attributes :: AttrMap
 attributes = attrMap defAttr [
@@ -213,22 +333,23 @@ attributes = attrMap defAttr [
                 (miscPanelSelectedScheme, withStyle (red `on` white) bold),
                 (miscPanelScheme, yellow `on` black),
                 (breakpointScheme, white `on` red),
+                (executionLineScheme, white `on` black),
                 (headerLabelScheme, withStyle (withStyle (blue `on` white) bold) underline)
              ]
 
 constructCodeStructure :: [String] -> Int -> [(String, Int)]
-constructCodeStructure [] _ = [] 
+constructCodeStructure [] _ = []
 constructCodeStructure (l : ls) c = (l, c) : constructCodeStructure ls (c + 1)
 
 initState :: [String] -> String -> ProgState
 initState codeLines commands = ProgState {
-                                    _focusRing = F.focusRing [CodeView, StackView, VariablesView, CommandsView], 
-                                    codeLines = constructCodeStructure codeLines 1, 
+                                    _focusRing = F.focusRing [CodeView, StackView, VariablesView, CommandsView],
+                                    codeLines = constructCodeStructure codeLines 1,
                                     selectedElement = 1,
-                                    breakpoints = empty,
+                                    breakpoints = Data.Set.empty,
                                     commandsViewContent = commands
                                 }
-                      
+
 ui :: IO ()
 ui = do
         args <- getArgs
