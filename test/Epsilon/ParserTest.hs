9a7723f2667c7a7114098f5de2312214107e0fcf
Hema Thota <hthota@ieng6-640.ucsd.edu>
2021-12-08 19:19:45 -0800

    added more test cases

--- test/Epsilon/ParserTest.hs
+++ test/Epsilon/ParserTest.hs
@@ -54,3 +54,38 @@ testStringValP = testGroup "stringValP"
   [ testCase "simple string literal" $ assertParser "\"hello world\"" stringValP (StringVal "hello world")
   , testCase "simple string literal with escape sequences" $ assertParser "\"hello \\\"\n world\"" stringValP (StringVal "hello \"\n world")
   ]
+
+testVarP :: TestTree
+testVarP = testGroup "testVarP"
+  [ testCase "simple variable with 1 character" $ assertParser "a" varP (var x)
+  , testCase "simple string with multiple characters" $ assertParser "myvar" varP (var "myvar")
+  ]
+
+testunOpP :: TestTree
+testunOpP = testGroup "testunOpP"
+  [ testCase "unary operator not" $ assertParser "not" unOpP (Not)
+  ]
+
+testbinOpP :: TestTree
+testbinOpP = testGroup "testbinOpP"
+  [ testCase "binary operator Add" $ assertParser "+" binOpP (Add)
+    testCase "binary operator Sub" $ assertParser "-" binOpP (Sub)
+    testCase "binary operator Mul" $ assertParser "*" binOpP (Mul)
+    testCase "binary operator Div" $ assertParser "/" binOpP (Div)
+    testCase "binary operator Gt"  $ assertParser ">" binOpP (Gt)
+    testCase "binary operator Gte" $ assertParser ">=" binOpP (Gte)
+    testCase "binary operator Lt"  $ assertParser "<" binOpP (Lt)
+    testCase "binary operator Lte" $ assertParser "<=" binOpP (Lte)
+    testCase "binary operator And" $ assertParser "&&" binOpP (And)
+    testCase "binary operator Or"  $ assertParser "||" binOpP (Or)
+    testCase "binary operator Idx" $ assertParser "." binOpP (Idx)
+  ]
+
+testopExp :: TestTree
+testopExp = testGroup "testopExp"
+  [
+    testCase "simple binary expression add numbers" $ assertParser "5 + 3" BinOpExpr (IntVal 5) Add (IntVal 3)
+    testCase "simple binary expression sub numbers" $ assertParser "5 - 3" BinOpExpr (IntVal 5) Sub (IntVal 3)
+    testCase "simple binary expression add variables" $ assertParser "x + y" BinOpExpr (var x) Add (var y)
+    testCase "simple binary expression add number and variable" $ assertParser "x + 3" BinOpExpr (var x) Sub (IntVal 3)
+  ]
