a851f3326cecc4f9deca72a1e33de85055724eae
DanielK2877 <200402837@newton.k12.ma.us>
2021-12-09 17:09:39 -0800

    Right name on fmap

--- src/Epsilon/Evaluator.hs
+++ src/Epsilon/Evaluator.hs
@@ -45,7 +45,7 @@ getStateFrames :: DState -> [String]
 getStackFrames dstate = let estate = (state dstate) in 
                         let pointerList = (stack estate)
                             dict = (memory  estate) in
-                                fmap (getNameFromPtr memory) pointerList
+                                fmap (getNameFromPtr dict) pointerList
 
 
 
